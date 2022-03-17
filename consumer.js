eaa155085d2fa3b3cf1aaf190743e53c2c3a6218
Gerhard Molin <gerhard@minimumbadass.com>
2022-03-17 18:31:59 +0200

    added consumer

---
+++ consumer.js
@@ -0,0 +1,38 @@
+const { Kafka } = require('kafkajs');
+
+
+const establishConsumer = async () => {
+	const kafka = await new Kafka({
+		"clientId": "myApp",
+		"brokers": ["localhost:9092"],
+	});
+
+	const kafkaConsumer = kafka.consumer({
+		"groupId": "test",
+	});
+	console.log('Connecting ...');
+	await kafkaConsumer.connect();
+	console.log('Connectd!');
+
+	await kafkaConsumer.subscribe({
+		"topic": "Users",
+		"fromBeginning": true,
+	});
+
+	await kafkaConsumer.run({
+		"eachMessage": async result => {
+			console.log(`'received message: ${result.message.value} on partiion ${result.partition}`);
+		}
+	})
+}
+
+const runMain = async () => {
+	try {
+		await establishConsumer();
+	} catch (error) {
+		console.log('error', error);
+		process.exit(1);
+	}
+}
+
+runMain();
